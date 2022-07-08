de66100e60c3e6a47d3781bbfd452175b732eb71
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 22:13:13 +0530

    creating dao classes

---
+++ src/main/java/lk/ijse/carrent/dto/Bookingdao.java
@@ -0,0 +1,4 @@
+package lk.ijse.carrent.dto;
+
+public class Bookingdao {
+}
--- src/main/java/lk/ijse/carrent/dto/Customerdao.java
+++ src/main/java/lk/ijse/carrent/dto/Customerdao.java
@@ -1,7 +1,17 @@
 package lk.ijse.carrent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Customerdao {
     String cusID;
+    String driverID;
     String name;
     String Address;
     String IDcard;
