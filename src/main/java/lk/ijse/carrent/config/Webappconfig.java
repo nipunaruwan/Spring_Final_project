3e930e5d5e89147eb86848af27949e090522e48b
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 19:35:34 +0530

    modelmapper

--- src/main/java/lk/ijse/carrent/config/Webappconfig.java
+++ src/main/java/lk/ijse/carrent/config/Webappconfig.java
@@ -1,11 +1,29 @@
 package lk.ijse.carrent.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.carrent")
 @EnableWebMvc
 public class Webappconfig {
+
+
+    @Bean
+public ModelMapper modelMapper(){
+        return new ModelMapper();
+
+}
+@Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+}
+
+
+    private class ModelMapper {
+    }
 }
--- src/main/java/lk/ijse/carrent/config/Webrootconfig.java
+++ src/main/java/lk/ijse/carrent/config/Webrootconfig.java
@@ -1,7 +1,10 @@
 package lk.ijse.carrent.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(JPAConfig.class)
 public class Webrootconfig {
+
 }
