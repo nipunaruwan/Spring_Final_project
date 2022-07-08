0aaf210fe6465ee6df3f99d764bcbf2192e7798d
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 22:42:01 +0530

    create Rent dao encapsulation

--- src/main/java/lk/ijse/carrent/dto/Rentdao.java
+++ src/main/java/lk/ijse/carrent/dto/Rentdao.java
@@ -1,4 +1,20 @@
 package lk.ijse.carrent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Rentdao {
+   String cusID;
+   String  DID;
+   String VID;
+   String Distance;
+   String Km;
+   double price ;
+
 }
