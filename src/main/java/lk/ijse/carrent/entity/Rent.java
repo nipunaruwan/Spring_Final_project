9481b7456b2dab8ac59347f4ece0e14c1fa4e30a
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-09 00:32:32 +0530

    Created Rent Entity Class

--- src/main/java/lk/ijse/carrent/entity/Rent.java
+++ src/main/java/lk/ijse/carrent/entity/Rent.java
@@ -1,4 +1,25 @@
 package lk.ijse.carrent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Rent {
+    @Id
+
+    String cusID;
+    String  DID;
+    String VID;
+    String Distance;
+    String Km;
+    double price ;
 }
