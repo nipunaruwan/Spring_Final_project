859fba342a7bb34bb3d3d8e8920243392866b60a
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 15:40:29 +0530

    webappinitailizer setup

--- src/main/java/lk/ijse/carrent/WebAppInitializer.java
+++ src/main/java/lk/ijse/carrent/WebAppInitializer.java
@@ -1,17 +1,19 @@
 package lk.ijse.carrent;
 
+import lk.ijse.carrent.config.Webappconfig;
+import lk.ijse.carrent.config.Webrootconfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{Webappconfig.class};
     }
 
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{Webrootconfig.class};
     }
 
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
