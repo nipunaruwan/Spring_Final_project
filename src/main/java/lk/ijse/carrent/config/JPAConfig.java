0ceebe9c516616d4961287e1661ebf421ec44e8a
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 20:12:17 +0530

    set transtrction Method

--- src/main/java/lk/ijse/carrent/config/JPAConfig.java
+++ src/main/java/lk/ijse/carrent/config/JPAConfig.java
@@ -6,13 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.activation.DataSource;
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement
@@ -21,24 +24,34 @@ import javax.activation.DataSource;
 public class JPAConfig {
 
 
-   @Autowired
-   Environment environment;
+    @Autowired
+    Environment environment;
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va) {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(ds);
         factoryBean.setJpaVendorAdapter(va);
         factoryBean.setPackagesToScan(environment.getRequiredProperty("entity.package"));
         return factoryBean;
     }
+
     @Bean
-    public JpaVendorAdapter vendorAdapter(){
+    public JpaVendorAdapter vendorAdapter() {
         HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
         va.setDatabasePlatform(environment.getRequiredProperty("db.dialect"));
         va.setDatabase(Database.MYSQL);
         va.setGenerateDdl(true);
         va.setShowSql(true);
-
-
+        return va;
     }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+
+
+}
