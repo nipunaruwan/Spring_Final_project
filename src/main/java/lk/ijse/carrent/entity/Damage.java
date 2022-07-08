7915abbb0c2cc514258a09020d011f3665ca6ade
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-09 00:07:59 +0530

    created Damege Entity class

--- src/main/java/lk/ijse/carrent/entity/Damage.java
+++ src/main/java/lk/ijse/carrent/entity/Damage.java
@@ -1,4 +1,24 @@
 package lk.ijse.carrent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Damage {
+    @Id
+
+    String VID;
+    String DVownname;
+    String VDiscription;
+    String Damagetype;
+    int Dvcontact;
 }
