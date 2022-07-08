fbaf8a1b50a7a0329d944f968e4ec0faf24da829
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 22:33:56 +0530

    create Employee dao encapsulation

--- src/main/java/lk/ijse/carrent/dto/Employeedao.java
+++ src/main/java/lk/ijse/carrent/dto/Employeedao.java
@@ -1,4 +1,20 @@
 package lk.ijse.carrent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Employeedao {
+    String EID;
+    String Ename;
+    String Email;
+    String password;
+    String Username;
+    String Jobtype;
+    int Contact;
 }
