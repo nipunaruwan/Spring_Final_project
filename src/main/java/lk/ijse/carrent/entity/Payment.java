bdde1d6829e180bb86f3804beeea0a16f3ce4243
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-09 00:24:56 +0530

    created Employee Entity class

--- src/main/java/lk/ijse/carrent/entity/Payment.java
+++ src/main/java/lk/ijse/carrent/entity/Payment.java
@@ -1,4 +1,23 @@
 package lk.ijse.carrent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Payment {
+    @Id
+    String paymentID;
+    String BookingID;
+    String time;
+    String date;
+    double Discount;
 }
