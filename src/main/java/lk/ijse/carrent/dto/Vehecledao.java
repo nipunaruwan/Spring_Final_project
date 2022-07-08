d218c9ba97adb275a2671f45db24a4d34d1dccbb
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 22:47:02 +0530

    create Vehicle dao encapsulation

--- src/main/java/lk/ijse/carrent/dto/Vehecledao.java
+++ src/main/java/lk/ijse/carrent/dto/Vehecledao.java
@@ -1,4 +1,13 @@
 package lk.ijse.carrent.dto;
 
 public class Vehecledao {
+  String  VID;
+  String  DID;
+  String  Brand;
+  String  rate;
+  String  fueltype;
+  String  image;
+  String  Distance;
+  int  ownercontact;
+  String  Vtype;
 }
