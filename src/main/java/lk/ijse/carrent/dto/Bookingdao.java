0b0cf8c1d41537fabe47fb94a2359dbfc8ace635
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 22:23:51 +0530

    create Booking class encapsulation

--- src/main/java/lk/ijse/carrent/dto/Bookingdao.java
+++ src/main/java/lk/ijse/carrent/dto/Bookingdao.java
@@ -1,4 +1,21 @@
 package lk.ijse.carrent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Bookingdao {
+    String BookingID;
+    String CusID;
+    String DriverID;
+    String pickuplocation;
+    String pickupDate;
+    String ReturnDate;
+    String ReseveDate;
+
 }
