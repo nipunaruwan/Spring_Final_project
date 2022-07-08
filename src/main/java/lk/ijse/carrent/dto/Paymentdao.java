c0d9843fe9588411f9ddc180e66ee3adad81443a
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-08 22:38:33 +0530

    create paymentdao class encapsulation

--- src/main/java/lk/ijse/carrent/dto/Paymentdao.java
+++ src/main/java/lk/ijse/carrent/dto/Paymentdao.java
@@ -1,4 +1,19 @@
 package lk.ijse.carrent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Paymentdao {
+   String paymentID;
+   String BookingID;
+   String time;
+   String date;
+   double Discount;
+
 }
