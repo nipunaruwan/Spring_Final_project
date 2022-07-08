25d1d0bc2c270b4c9dcabdb63c84330bbdc41efb
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-09 00:35:05 +0530

    created Vehicle Entity class

--- src/main/java/lk/ijse/carrent/entity/Vehicle.java
+++ src/main/java/lk/ijse/carrent/entity/Vehicle.java
@@ -1,4 +1,27 @@
 package lk.ijse.carrent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Vehicle {
+    @Id
+    String  VID;
+    String  DID;
+    String  Brand;
+    String  rate;
+    String  fueltype;
+    String  image;
+    String  Distance;
+    int  ownercontact;
+    String  Vtype;
 }
