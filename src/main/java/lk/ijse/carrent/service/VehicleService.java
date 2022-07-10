c011f4afaca7fa8b18fcc2ae35b10c78d93e5f69
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-10 16:24:25 +0530

    setup vehicleService interface

--- src/main/java/lk/ijse/carrent/service/VehicleService.java
+++ src/main/java/lk/ijse/carrent/service/VehicleService.java
@@ -1,5 +1,15 @@
 package lk.ijse.carrent.service;
 
+import lk.ijse.carrent.dto.Rentdto;
+import lk.ijse.carrent.dto.Vehecledto;
+
+import java.util.List;
+
 public interface
 VehicleService {
+    void saveVehicle(Vehecledto dto);
+    void deleteVehicle(String id);
+    void updateVehicle(Vehecledto dto);
+    void  searchVehicle(String id);
+    List<Vehecledto> getAllVehicles();
 }
