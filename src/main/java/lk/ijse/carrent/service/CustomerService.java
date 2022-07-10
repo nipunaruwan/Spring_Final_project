cbd0be022d6806f1ed6d1961bbca51aff01c440f
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-10 13:47:15 +0530

    set customer service

--- src/main/java/lk/ijse/carrent/service/CustomerService.java
+++ src/main/java/lk/ijse/carrent/service/CustomerService.java
@@ -1,4 +1,14 @@
 package lk.ijse.carrent.service;
 
+import lk.ijse.carrent.dto.Bookingdto;
+import lk.ijse.carrent.dto.Customerdto;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(Customerdto dto);
+    void deleteCustomer(String id);
+    void updateCustomer(Customerdto dto);
+   void   searchCustomer(String id);
+    List<Customerdto> getAllCustomers();
 }
