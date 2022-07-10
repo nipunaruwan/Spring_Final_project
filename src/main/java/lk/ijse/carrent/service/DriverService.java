4fb682dd4441d2b4d5183ee70c349c6e5830450b
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-10 16:12:27 +0530

    setup DriverService interface

--- src/main/java/lk/ijse/carrent/service/DriverService.java
+++ src/main/java/lk/ijse/carrent/service/DriverService.java
@@ -1,4 +1,16 @@
 package lk.ijse.carrent.service;
 
+import lk.ijse.carrent.dto.Customerdto;
+import lk.ijse.carrent.dto.Damagedto;
+import lk.ijse.carrent.dto.Driverdto;
+
+import java.util.List;
+
 public interface DriverService {
+
+    void saveDriver(Driverdto dto);
+    void deleteDriver(String id);
+    void updateDriver(Driverdto dto);
+    void  searchDriver(String id);
+    List<Customerdto> getAllDrivers();
 }
