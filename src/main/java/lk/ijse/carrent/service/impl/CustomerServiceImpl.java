4c053808b9bb1ca7c82f60e085504352122a5e9f
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-11 01:21:38 +0530

    customer save method set

--- src/main/java/lk/ijse/carrent/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/carrent/service/impl/CustomerServiceImpl.java
@@ -1,13 +1,29 @@
 package lk.ijse.carrent.service.impl;
 
 import lk.ijse.carrent.dto.Customerdto;
+import lk.ijse.carrent.entity.Customer;
+import lk.ijse.carrent.repo.CustomerRepo;
 import lk.ijse.carrent.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepo repo;
+
+    @Autowired
+    private ModelMapper map;
     public void saveCustomer(Customerdto dto) {
 
+        if (!repo.existsById(dto.getCusID())) {
+            repo.save(map.map(dto, Customer.class));
+        } else {
+            throw new RuntimeException("This cusiD id is already exists....!!!");
+        }
     }
 
     public void deleteCustomer(String id) {
