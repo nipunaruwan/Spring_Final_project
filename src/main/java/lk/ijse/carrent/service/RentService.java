5579ceaac2166695202c001fcbda121efe407790
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-10 16:22:11 +0530

    setup RentService interface

--- src/main/java/lk/ijse/carrent/service/RentService.java
+++ src/main/java/lk/ijse/carrent/service/RentService.java
@@ -1,4 +1,14 @@
 package lk.ijse.carrent.service;
 
+import lk.ijse.carrent.dto.Paymentdto;
+import lk.ijse.carrent.dto.Rentdto;
+
+import java.util.List;
+
 public interface RentService {
+    void saveRent(Rentdto dto);
+    void deleteRent(String id);
+    void updateRent(Rentdto dto);
+    void  searchRent(String id);
+    List<Rentdto> getAllRents();
 }
