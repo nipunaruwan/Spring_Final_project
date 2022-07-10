18997fdba216358075926d0e2f47dfdaaf3914b2
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-11 00:44:23 +0530

    extends JpaRepository for Driverrepo

--- src/main/java/lk/ijse/carrent/repo/DriverRepo.java
+++ src/main/java/lk/ijse/carrent/repo/DriverRepo.java
@@ -1,4 +1,7 @@
 package lk.ijse.carrent.repo;
 
-public interface DriverRepo {
+import lk.ijse.carrent.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepo extends JpaRepository<Driver,String> {
 }
