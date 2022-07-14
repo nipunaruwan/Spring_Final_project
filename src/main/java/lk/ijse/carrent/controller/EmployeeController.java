3c98e2f7c60e3acfb210bc5c88ad2adee387caa1
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-14 17:37:22 +0530

    completed employee controller

--- src/main/java/lk/ijse/carrent/controller/EmployeeController.java
+++ src/main/java/lk/ijse/carrent/controller/EmployeeController.java
@@ -1,4 +1,46 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.Customerdto;
+import lk.ijse.carrent.dto.Employeedto;
+import lk.ijse.carrent.service.EmployeeService;
+import lk.ijse.carrent.utill.Responceutil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("Employee")
+@CrossOrigin
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil SaveEmployee(Employeedto employeedto){
+        employeeService.saveEmployee(employeedto);
+        return new Responceutil(200, "save", null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil updateEmployee(@RequestBody Employeedto employeedto) {
+        employeeService.updateEmployee(employeedto);
+        return new Responceutil(200, "update", null);
+    }
+
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil DeleteEmployee(@RequestParam String iD) {
+        employeeService.deleteEmployee(iD);
+        return new Responceutil(200, "delete", null);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil getAllEmployee() {
+        return new Responceutil(200, "all", employeeService.getAllEmployees());
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil SearchEmployee(@PathVariable String iD) {
+        return new Responceutil(200, "search", employeeService.searchEmployee(iD));
+    }
+
 }
