a5ffe1a9ddc5861f97fd983645edf92612dc0ea6
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-28 09:36:04 +0530

    damage controller set

--- src/main/java/lk/ijse/carrent/controller/DamageController.java
+++ src/main/java/lk/ijse/carrent/controller/DamageController.java
@@ -1,4 +1,24 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.Customerdto;
+import lk.ijse.carrent.dto.Damagedto;
+import lk.ijse.carrent.service.CustomerService;
+import lk.ijse.carrent.service.DamageService;
+import lk.ijse.carrent.utill.Responceutil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class DamageController {
+
+    @Autowired
+    DamageService damageService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil SaveDamage(Damagedto damagedto) {
+        damageService.saveDamage(damagedto);
+        return new Responceutil(200, "save", null);
+
+    }
+
 }
