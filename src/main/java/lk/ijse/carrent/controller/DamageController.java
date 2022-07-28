4bab4c74415276327fb4ecd9e6a8bb29dfc08f63
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-28 09:57:14 +0530

    payment controller added

--- src/main/java/lk/ijse/carrent/controller/DamageController.java
+++ src/main/java/lk/ijse/carrent/controller/DamageController.java
@@ -7,8 +7,10 @@ import lk.ijse.carrent.service.DamageService;
 import lk.ijse.carrent.utill.Responceutil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("Damage")
+@CrossOrigin
 public class DamageController {
 
     @Autowired
@@ -20,5 +22,27 @@ public class DamageController {
         return new Responceutil(200, "save", null);
 
     }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil Upadatedamage(@RequestBody Damagedto damagedto) {
+        damageService.updateDamage(damagedto);
+        return new Responceutil(200, "update", null);
+    }
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil DeleteDamage(@RequestParam String iD) {
+        damageService.deleteDamage(iD);
+        return new Responceutil(200, "delete", null);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil getAllDamage() {
+        return new Responceutil(200, "all", damageService.getAllDamages());
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil SearchCustomer(@PathVariable String iD) {
+        return new Responceutil(200, "search", damageService.searchDamage(iD));
+    }
+
 
 }
--- src/main/java/lk/ijse/carrent/controller/paymentController.java
+++ src/main/java/lk/ijse/carrent/controller/paymentController.java
@@ -1,6 +1,47 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.Customerdto;
+import lk.ijse.carrent.dto.Paymentdto;
+import lk.ijse.carrent.service.CustomerService;
+import lk.ijse.carrent.service.PaymentService;
+import lk.ijse.carrent.utill.Responceutil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("Payment")
+@CrossOrigin
 public class paymentController {
+    @Autowired
+    PaymentService paymentService;
 
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil SavePayment(Paymentdto paymentdto) {
+        paymentService.savePayment(paymentdto);
+        return new Responceutil(200, "save", null);
+
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil updatepayment(@RequestBody Paymentdto paymentdto) {
+        paymentService.updatePayment(paymentdto);
+        return new Responceutil(200, "update", null);
+    }
+
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil Deletepayment(@RequestParam String iD) {
+        paymentService.deletePayment(iD);
+        return new Responceutil(200, "delete", null);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil getAllpayment() {
+        return new Responceutil(200, "all", paymentService.getAllPayments());
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Responceutil Searchpayment(@PathVariable String iD) {
+        return new Responceutil(200, "search", paymentService.searchPayment(iD));
+    }
 
 }
